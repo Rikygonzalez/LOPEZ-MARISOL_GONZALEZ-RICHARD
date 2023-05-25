8197e3d63bc299f3a7be6781d25437de43d215f1
Richard Gonzalez <rikygonzalez09@gmail.com>
2023-05-25 19:57:25 -0300

    Se creo la interface IDao

---
+++ LOPEZ-MARISOL_GONZALEZ-RICHARD/src/com/backend/dao/impl/OdontologoDaoH2.java
@@ -0,0 +1,15 @@
+package com.backend.dao.impl;
+
+import org.apache.log4j.Logger;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OdontologoDaoH2 implements IDao<Odontologo> {
+
+    private static final Logger LOGGER = Logger.getLogger(OdontologoDaoH2.class);
+
+
+
+}
