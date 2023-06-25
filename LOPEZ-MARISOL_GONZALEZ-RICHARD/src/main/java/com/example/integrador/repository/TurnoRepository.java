bb55a5093ec6a854297c5fe4e210778d8d532e45
Richard Gonzalez <rikygonzalez09@gmail.com>
2023-06-25 20:33:53 -0300

    Se creó el archivo ITurno service con su contenido, TurnoRepository con su contenido, BadRequestException con su contenido y TurnoService con su contenido

---
+++ LOPEZ-MARISOL_GONZALEZ-RICHARD/src/main/java/com/example/integrador/repository/TurnoRepository.java
@@ -0,0 +1,7 @@
+package com.example.integrador.repository;
+
+import com.example.integrador.entity.Turno;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TurnoRepository extends JpaRepository<Turno, Long> {
+}
