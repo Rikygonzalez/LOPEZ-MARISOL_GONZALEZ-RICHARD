9d5b2410dfae70288658ebd0dfd8296561ac6880
Richard Gonzalez <rikygonzalez09@gmail.com>
2023-06-25 20:46:41 -0300

    Se creó y completó el contenido de TurnoController

---
+++ LOPEZ-MARISOL_GONZALEZ-RICHARD/src/main/java/com/example/integrador/controller/TurnoController.java
@@ -0,0 +1,49 @@
+package com.example.integrador.controller;
+
+import com.example.integrador.dto.TurnoDto;
+import com.example.integrador.entity.Turno;
+import com.example.integrador.exceptions.BadRequestException;
+import com.example.integrador.exceptions.ResourceNotFoundException;
+import com.example.integrador.service.ITurnoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/turnos")
+public class TurnoController {
+    private final ITurnoService turnoService;
+
+    @Autowired
+    public TurnoController(ITurnoService turnoService) {
+        this.turnoService = turnoService;
+    }
+
+    //GET
+    @GetMapping("/{id}")
+    public ResponseEntity<?> buscarTurnoPorId(@PathVariable Long id){
+        return new ResponseEntity<>(turnoService.buscarTurnoPorId(id), null, HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public List<TurnoDto> listarTurnos(){
+        return turnoService.listarTodos();
+    }
+
+    //POST
+    @PostMapping("/registrar")
+    public ResponseEntity<TurnoDto> registrarTurno(@RequestBody Turno turno) throws BadRequestException{
+        return new ResponseEntity<>(turnoService.registrarTurno(turno), null, HttpStatus.CREATED);
+    }
+
+    //DELETE
+    @DeleteMapping("/emilinar/{id}")
+    public ResponseEntity<?> eliminarTurno(@PathVariable Long id) throws ResourceNotFoundException{
+        turnoService.eliminarTurno(id);
+        return ResponseEntity.ok("Turno eliminado");
+    }
+}
